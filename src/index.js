9265a4397ff0578534d2501d9db4d5fb07223ccc
Megan Farley <megfarley4@gmail.com>
2018-11-27 10:15:09 -0600

    coming along

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
+const tableBody = document.querySelector('#table-body')
+const ce = document.createElement.bind(document)
 
-})
+
+const render = function(){
+
+  fetch('http://localhost:3000/dogs')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(dogs){
+      console.log(dogs)
+      renderDogs(dogs)
+    })
+
+}
+
+const renderDogs = function(dogs){
+  dogs.forEach(function(dog){
+    console.log(dog)
+    const dogRow = ce('tr')
+    const dogName = ce('td')
+    dogName.innerHTML = dog.name
+    const dogBreed = ce('td')
+    dogBreed.innerHTML = dog.breed
+    const dogSex = ce('td')
+    dogSex.innerHTML = dog.sex
+    const dogButton = ce('td')
+    const button = ce('button')
+    button.innerHTML = 'Edit Dog'
+    // dogRow.innerHTML = `
+    //   <td></td>
+    //   <td></td>
+    //   <td></td>
+    //   <td></td>
+    // `
+    tableBody.append(dogRow)
+    dogRow.append(dogName)
+    dogRow.append(dogBreed)
+    dogRow.append(dogSex)
+    dogRow.append(dogButton)
+    dogButton.append(button)
+  })
+};
+
+render()
