cba510f15b70b5ac9f47eecf2c1dc9cb7ad9a1e1
ch0mper <hannahchang17@gmail.com>
2018-11-27 10:40:12 -0600

    everything but patch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
+fetch('http://localhost:3000/dogs')
+  .then(function(response) {
+    return response.json()
+  })
+  .then (function(dogs) {
+    render(dogs)
+  })
 
-})
+const dogTable = document.body.querySelector('#table-body')
+const submitButton = document.querySelector('input[type="submit"]')
+const dogNameField = document.querySelector('input[name="name"]')
+const dogBreedField = document.querySelector('input[name="breed"]')
+const dogSexField = document.querySelector('input[name="sex"]')
+
+const render = (dogs) => {
+  dogs.forEach(function(dog) {
+    let dogTR = document.createElement('tr')
+    let dogName = document.createElement('td')
+    dogName.innerText = `${dog.name}`
+    dogTR.append(dogName)
+
+    let dogBreed = document.createElement('td')
+    dogBreed.innerText = `${dog.breed}`
+    dogTR.append(dogBreed)
+
+    let dogSex = document.createElement('td')
+    dogSex.innerText = `${dog.sex}`
+    dogTR.append(dogSex)
+
+    let editDogTD = document.createElement('td')
+    let editDogButton = document.createElement('button')
+    editDogButton.innerText = `Edit ${dog.name}`
+    editDogButton.style.width = '150px'
+    editDogTD.append(editDogButton)
+    dogTR.append(editDogTD)
+
+    dogTable.append(dogTR)
+
+    editDogButton.addEventListener('click', function() {
+      dogNameField.value = dog.name
+      dogBreedField.value = dog.breed
+      dogSexField.value = dog.sex
+
+      submitButton.addEventListener('click', function(e) {
+        e.preventDefault()
+        dogName.innerText = dogNameField.value
+        dogBreed.innerText = dogBreedField.value
+        dogSex.innerText = dogSexField.value
+        editDB(dog);
+      })
+    })
+
+  })
+}
+
+const editDB = (dog) => {
+  // patch to server
+  dogNameField.value = ''
+  dogNameField.value = ''
+  dogNameField.value = ''
+}
